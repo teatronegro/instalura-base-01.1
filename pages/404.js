55894246e8c3ff6fffe9e114fa686ed122c78dff
LUIZ AUGUSTO OLIVEIRA FERREIRA <78542462+teatronegro@users.noreply.github.com>
2021-02-15 09:59:00 -0300

    JAMSTACKBootCamp01
    JAMSTACKBootCamp01.1

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function page404() {
+    return (
+<div>
+    pagina 404
+</div>
+
+    )
+}
---
+++ pages/sobre.js
@@ -0,0 +1,8 @@
+export default function PageSobre() {
+    return (
+<div>
+    Pagina Sobre
+</div>
+
+    )
+}
